b8170125aaf65d91796697471947af5b72cee11c
Alberto Matties <new_alberto_dl@yahoo.com.br>
2017-06-18 21:22:27 -0300

    Projeto base criado em http://start.spring.io/

---
+++ src/main/java/com/spingCrud/AlunoManager/AlunoManagerApplication.java
@@ -0,0 +1,12 @@
+package com.spingCrud.AlunoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoManagerApplication.class, args);
+	}
+}
